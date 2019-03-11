da7091ccdff119229299cf96b46017ef1757d25f
GBTC408003UR <GBTC408003UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-03-11 15:22:31 -0400

    Passing two variable to template

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @RequestMapping("/")
 
-    public String homePage(Model model){
+    public String homePage(Model model, Model model2){
         model.addAttribute("myvar","Say hello to the people.");
+        //Second variable being submitted
+        model2.addAttribute("myvar2", "Hi people");
         return "hometemplate";
     }
 }
